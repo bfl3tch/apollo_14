0db592f00361552bccb886de3ea99b6cad7e20e1
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-07-22 21:21:25 -0400

    User story 3 complete

--- spec/features/astronauts/index_spec.rb
+++ spec/features/astronauts/index_spec.rb
@@ -6,6 +6,21 @@ RSpec.describe 'The astronaut index page' do
     @astronaut2 = Astronaut.create!(name: 'Buzz Aldrin', age: 40, job: 'Commander')
     @astronaut3 = Astronaut.create!(name: 'Chris Cassidy', age: 38, job: 'Commander')
 
+    @mission1 = Mission.create!(title: 'Apollo 13', time_in_space: 42)
+    @mission2 = Mission.create!(title: 'Capricorn 4', time_in_space: 64)
+    @mission3 = Mission.create!(title: 'Gemini 7', time_in_space: 79)
+    @mission4 = Mission.create!(title: 'Zeus 10', time_in_space: 98)
+    @mission5 = Mission.create!(title: 'Pluto 1', time_in_space: 104)
+
+    @astromission1 = AstronautMission.create(astronaut: @astronaut1, mission: @mission1)
+    @astromission2 = AstronautMission.create(astronaut: @astronaut1, mission: @mission2)
+    @astromission3 = AstronautMission.create(astronaut: @astronaut1, mission: @mission3)
+    @astromission4 = AstronautMission.create(astronaut: @astronaut2, mission: @mission4)
+    @astromission5 = AstronautMission.create(astronaut: @astronaut2, mission: @mission5)
+    @astromission6 = AstronautMission.create(astronaut: @astronaut3, mission: @mission1)
+    @astromission7 = AstronautMission.create(astronaut: @astronaut3, mission: @mission2)
+    @astromission8 = AstronautMission.create(astronaut: @astronaut3, mission: @mission3)
+
     visit '/astronauts'
   end
 
@@ -23,5 +38,10 @@ RSpec.describe 'The astronaut index page' do
 
   it 'shows the average age of all the astronauts' do
     expect(page).to have_content("Average age for all astronauts: 41")
+  end
+
+  it 'lists the associated space missions in alphabetical order for each astronaut' do
+    expect(@mission1.title).to appear_before(@mission2.title)
+    expect(@mission2.title).to appear_before(@mission3.title)
   end
 end
--- spec/models/mission_spec.rb
+++ spec/models/mission_spec.rb
@@ -10,4 +10,21 @@ describe Mission, type: :model do
     it { should have_many :astronaut_missions}
     it { should have_many :astronauts}
   end
+
+  describe 'class methods' do
+    before(:each) do
+      @mission1 = Mission.create!(title: 'Apollo 13', time_in_space: 42)
+      @mission2 = Mission.create!(title: 'Capricorn 4', time_in_space: 64)
+      @mission3 = Mission.create!(title: 'Gemini 7', time_in_space: 79)
+      @mission4 = Mission.create!(title: 'Zeus 10', time_in_space: 98)
+      @mission5 = Mission.create!(title: 'Pluto 1', time_in_space: 104)
+    end
+
+    describe '::alphabatize' do
+      it 'sorts the missions by name' do
+        expected = [@mission1, @mission2, @mission3, @mission5, @mission4]
+        expect(Mission.alphabatize).to eq(expected)
+      end
+    end
+  end
 end
